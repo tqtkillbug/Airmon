99e718f3753bdc528b14fa62f75ff863b681b37c
TienDapChai <tqtteams1st@gmail.com>
2024-02-09 17:39:27 +0700

    Update pom

--- src/main/java/com/tqt/airmon/model/AirProject.java
+++ src/main/java/com/tqt/airmon/model/AirProject.java
@@ -1,15 +1,15 @@
 package com.tqt.airmon.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import lombok.Data;
 
 import java.util.ArrayList;
